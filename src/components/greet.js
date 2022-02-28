7dc9c73d7116dbe9a0212aec6c115dba7a3d2c9f
aafrinabid <mohdaafrin@gmail.com>
2022-02-28 20:29:56 +0530

    branches change

---
+++ src/components/greet.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const greet =()=><h1>hello aafrin</h1>
+export default greet
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+class welcome extends Component{
+    render(){
+        return <h1>welcome aafrin but from class</h1>
+    }
+}
+ export default welcome
